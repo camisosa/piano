26ba32d891b4e4b65b84f840744eacc4d1af66ea
camisosa <82180696+camisosa@users.noreply.github.com>
2024-09-08 03:19:28 -0300

    Update piano.js

--- piano.js
+++ piano.js
@@ -1,16 +1,16 @@
 // Secuencia de notas
-const secuenciaDeNotas = ["do", "do", "sol", "sol", "la", "la", "sol", "fa", "fa", "mi", "mi", "re", "re", "do"];
-let notaActual = 0;
+const secuenciaDeNotas = [
+    "do", "do", "sol", "sol", "la", "la", "sol", 
+    "fa", "fa", "mi", "mi", "re", "re", "do",
+    /*"sol", "sol", "fa", "fa", "mi", "mi", "re", 
+    "sol", "sol", "fa", "fa", "mi", "mi", "re"*/
+];
+
 
 // Cantidad de notas acertadas para cada nivel
-const ia1 = Math.ceil(secuenciaDeNotas.length / 3); // Primer tercio
-const ia2 = ia1 * 2; // Segundo tercio
-const ia3 = ia1*3 -1;
-
-let mensaje;
-
-let puedeReproducirSonido;
-
+const ia1 = Math.ceil(secuenciaDeNotas.length / 2); // Primer tercio
+const ia2 = ia1 * 2 - 1; // Segundo tercio
+const ia3 = secuenciaDeNotas.length; // Tercer tercio
 
 // Mapeo de teclas a notas
 const mapeoTeclas = {
@@ -20,15 +20,26 @@ const mapeoTeclas = {
     'y': 'fa',
     'u': 'sol',
     'i': 'la',
-    'o': 'si'
+    'o': 'si',
+    'E': 'do',
+    'R': 're',
+    'T': 'mi',
+    'Y': 'fa',
+    'U': 'sol',
+    'I': 'la',
+    'O': 'si'
 };
 
-const duracionNota = 200;
-
+//Tiempo estimado que dura cada nota
+const duracionNota = 150;
+// identificador del intervalo de la melodía
+let intervaloMelodia;
+//Controla por que nota va
+let notaActual = 0;
 // Reproduce el sonido de la nota
 function reproducirSonido(nota) {
     const audio = new Audio(`data/${nota}.mp3`);
-    audio.play().catch(error => console.log(error));
+    audio.play();
 }
 
 // Activa la tecla
@@ -37,7 +48,6 @@ function activarTecla(nota) {
     if (tecla) {
         tecla.classList.add('active'); // Rellena
         reproducirSonido(nota); // Reproducir el sonido
-
         // Saca el relleno
         setTimeout(() => {
             tecla.classList.remove('active');
@@ -45,44 +55,80 @@ function activarTecla(nota) {
     }
 }
 
+//Empieza a sonar la melodia
+
 function melodía() {
-    let intervaloMelodia = setInterval(() => {
+    mensajeDeConsola.forEach(mensajeDeConsola => {
+        mensajeDeConsola.style.display = 'none'; // Oculta todos los mensajes}
+    });
+    mensajeDeConsola[1].style.display = 'block'; // Mostrar el mensaje 
+
+    mensajeDeIa.forEach(mensajeDeIa => {
+        mensajeDeIa.style.display = 'none'; // Oculta todos los mensajes}
+    });
+    
+    intervaloMelodia = setInterval(() => {
         if (notaActual < secuenciaDeNotas.length) {
             const nota = secuenciaDeNotas[notaActual];
             activarTecla(nota);
-            console.log(`Nota tocada: ${nota}`); // Mensaje en la consola
             notaActual++;
         } else {
             clearInterval(intervaloMelodia); // Detener la melodía cuando haya terminado
-            puedeReproducirSonido = false; // Habilitar el control de teclado
             notaActual = 0; // Reiniciar para jugar de nuevo
+            document.addEventListener('keydown', teclaPresionada);
+            mensajeDeConsola.forEach(mensajeDeConsola => {
+                mensajeDeConsola.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeConsola[2].style.display = 'block'; // Mostrar el mensaje 
         }
     }, 500);
 }
 
+
+//Verifica si las teclas presionadas son las correctas
 function controlDeTeclaPresionada(nota) {
     const notaCorrecta = secuenciaDeNotas[notaActual];
+
     if (nota === notaCorrecta) { // Comparar los valores de las notas
         notaActual++;
         activarTecla(nota);
-        console.log(`Nota tocada: ${nota}`); // Mensaje en la consola
-        console.log(`Cantidad de notas: ${ia3} Cntidad de notas correctas: ${notaActual}`); // Mensaje en la consola
-        
-    } else{
-       
-        if (notaActual < ia1) {
-            mensaje = "¡Falta mucha práctica!";
+        if (notaActual == ia3) {
+            notaActual = 0;
+            mensajeDeConsola.forEach(mensajeDeConsola => {
+                mensajeDeConsola.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeConsola[5].style.display = 'block'; // Mostrar el mensaje 
+
+            mensajeDeIa.forEach(mensajeDeIa => {
+                mensajeDeIa.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeIa[2].style.display = 'block'; // Mostrar el mensaje 
+
         }
 
-        if (notaActual < ia2) {
-            mensaje = "¡Eres bueno, pero te falta un poco de práctica!";
+    } else {
+
+        if (notaActual < ia1) {
+            mensajeDeConsola.forEach(mensajeDeConsola => {
+                mensajeDeConsola.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeIa.forEach(mensajeDeIa => {
+                mensajeDeIa.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeIa[0].style.display = 'block'; // Mostrar el mensaje 
+            mensajeDeConsola[3].style.display = 'block'; // Mostrar el mensaje         
+
+        } else if (notaActual > ia1 && notaActual < ia2) {
+            mensajeDeConsola.forEach(mensajeDeConsola => {
+                mensajeDeConsola.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeIa.forEach(mensajeDeIa => {
+                mensajeDeIa.style.display = 'none'; // Oculta todos los mensajes}
+            });
+            mensajeDeIa[1].style.display = 'block'; // Mostrar el mensaje 
+            mensajeDeConsola[4].style.display = 'block'; // Mostrar el mensaje 
         }
-        
-        if (notaActual === ia3) {
-            mensaje = "¡Bien hecho!";
-            
-        }
-        alert(mensaje);
+
         notaActual = 0; // Reiniciar para jugar de nuevo
     }
 }
@@ -96,9 +142,98 @@ function teclaPresionada(event) {
     }
 }
 
-function estamosJugando() {
-    document.addEventListener('keydown', teclaPresionada);
+
+// Selecciona todos los elementos que tienen la clase "diapositiva"
+const diapositivas = document.querySelectorAll('.diapositiva');
+const mensajeDeConsola = document.querySelectorAll('.mensajeConsola');
+const mensajeDeIa = document.querySelectorAll('.ia');
+// Carga el boton siguiente
+const siguiente = document.querySelector('.siguiente');
+// Carga el boton siguiente
+const anterior = document.querySelector('.anterior');
+// Carga el boton apagado
+const apagado = document.querySelector('.apagado');
+// Carga el boton encendido
+const encendido = document.querySelector('.encendido');
+// Carga las instrucciones
+const instrucciones = document.querySelector('.carrusel');
+// Carga el piano
+const piano = document.querySelector('.piano-contenedor');
+// Carga la botonera piano
+const pianoMuestra = document.querySelector('.piano-muestra');
+
+
+// Controlar la imagen que muestra actual
+let indiceActual = 0;
+
+// Mostrar la diapositiva
+function mostrarDiapositiva(indice) {
+    diapositivas.forEach(diapositiva => {
+        diapositiva.style.display = 'none'; // Oculta todas las diapositivas
+        piano.style.display = 'none'; // Oculta todas las diapositivas
+    });
+
+    diapositivas[indice].style.display = 'block'; // Muestra la diapositiva actual
+
+    // Controlar la visibilidad de los botones
+    if (indiceActual === 0) {
+        anterior.style.display = 'none'; // Oculta el botón "Anterior"
+    } else {
+        anterior.style.display = 'block'; // Muestra el botón "Anterior"
+    }
+
+    if (indiceActual === diapositivas.length - 1) {
+        encendido.style.display = 'block'; // Mostrar el botón "ON" 
+        mensajeDeConsola[0].style.display = 'block'; // Mostrar el mensaje probemos
+        siguiente.style.display = 'none'; // Oculta el botón "Siguiente" 
+        apagado.style.display = 'none'; // Mostrar el botón "OFF" 
+    } else {
+        siguiente.style.display = 'block'; // Mostrar el botón "Siguiente"
+        encendido.style.display = 'none'; // Oculta el botón "ON" 
+        apagado.style.display = 'block'; // Mostrar el botón "OFF"        
+        mensajeDeConsola[0].style.display = 'none'; // Oculta el mensaje probemos
+    }
+}
+
+//Avanzar a la siguiente diapositiva
+function continuar() {
+    // Aumentar el índice
+    indiceActual++;
+    // Mostrar la nueva diapositiva
+    mostrarDiapositiva(indiceActual);
+}
+
+// Función para retroceder a la diapositiva anterior
+function volver() {
+    // Disminuir el índice
+    indiceActual--;
+    // Mostrar la nueva diapositiva
+    mostrarDiapositiva(indiceActual);
+}
+
+function encenderPiano() {
+    instrucciones.style.display = 'none'; // Oculta todas las diapositivas
+    encendido.style.display = 'none'; // Oculta el botón "ON" 
+    apagado.style.display = 'block'; // Mostrar el botón "OFF" 
+    anterior.style.display = 'none'; // Oculta el botón "Anterior"
+    piano.style.display = 'block'; // Muestra el piano
+    pianoMuestra.style.display = 'none'; // Muestra el piano
     melodía();
 }
 
-estamosJugando();
+function cancelar() {
+    if (indiceActual == diapositivas.length - 1) {
+        instrucciones.style.display = 'block'; // Mostrar las instrucciones
+        anterior.style.display = 'block'; // Mostrar el botón "Anterior"
+        encendido.style.display = 'block'; // Muestra el botón "ON" 
+        apagado.style.display = 'none'; // Mostrar el botón "OFF" 
+        piano.style.display = 'none'; // Oculta el piano
+        mensajeDeConsola.style.display = 'block'; // Mostrar el botón "ON" 
+        pianoMuestra.style.display = 'block'; // Muestra el piano
+        clearInterval(intervaloMelodia); // Detiene la reproducción de la melodía si está sonando
+        notaActual = 0; // Reiniciar la secuencia de notas
+    }     
+}
+
+
+mostrarDiapositiva(indiceActual);
